2c97cff8c28440a41b0e3c68a6160cec7b102950
BonSik <rnqhstlr2297@gmail.com>
2023-11-13 01:36:49 +0900

    test: Day->Date 클래스로 변경에 따른 테스트 변경

--- src/test/java/christmas/domain/DateTest.java
+++ src/test/java/christmas/domain/DateTest.java
@@ -7,13 +7,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-class DayTest {
+class DateTest {
 
     @DisplayName("날짜가 1~31일에 벗어날 경우 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(ints = {0, 32, -1, 33})
     void createDayOfOutOfRange1Between31(int day) {
-        assertThatThrownBy(() -> new Day(day))
+        assertThatThrownBy(() -> new Date(day))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ExceptionMessage.INVALID_DAY.getMessage());
     }
