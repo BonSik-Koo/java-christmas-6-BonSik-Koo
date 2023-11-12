45ae2c5426b51fb99a3d010d29451f169112b6f5
BonSik <rnqhstlr2297@gmail.com>
2023-11-13 01:35:09 +0900

    refactor: Day->Date 클래스로 변경

--- src/main/java/christmas/domain/Date.java
+++ src/main/java/christmas/domain/Date.java
@@ -2,12 +2,13 @@ package christmas.domain;
 
 import static christmas.ExceptionMessage.INVALID_DAY;
 
-public class Day {
+public class Date {
     private final int startDay = 1;
     private final int endDay = 31;
+    private final int month = 12;
     private final int day;
 
-    public Day(int day) {
+    public Date(int day) {
         validateDay(day);
         this.day = day;
     }
