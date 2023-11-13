0872ce95f474128d11d1143e19999d7289caaec3
BonSik <rnqhstlr2297@gmail.com>
2023-11-14 03:07:48 +0900

    feat: 예상 결제 금액 계산 구현 및 테스트

---
+++ src/main/java/christmas/domain/EventPanner.java
@@ -0,0 +1,21 @@
+package christmas.domain;
+
+public class EventPanner {
+    private final Date date;
+    private final OrderMenus orderMenus;
+    private final EventBenefit eventBenefit;
+
+    public EventPanner(Date date, OrderMenus orderMenus) {
+        this.date = date;
+        this.orderMenus = orderMenus;
+        this.eventBenefit = new EventBenefit(date, orderMenus);
+    }
+
+    public int getPaymentPrice() {
+        int totalOrderMenuPrice = orderMenus.calculateTotalPrice();
+        int totalDiscountPrice = eventBenefit.getTotalDiscountPrice();
+        return totalOrderMenuPrice - totalDiscountPrice;
+    }
+
+}
+
---
+++ src/test/java/christmas/domain/EventPannerTest.java
@@ -0,0 +1,37 @@
+package christmas.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class EventPannerTest {
+
+    @DisplayName("할인 후 예상 결제 금액을 계산한다.")
+    @Test
+    void calculatePaymentPrice() {
+        //given
+        final int predictionPaymentPrice = (5000 + 15000) - (4046 + 1000);
+        Date date = new Date(31); // 평일, 특별 할인
+
+        List<OrderMenu> menus = List.of(
+                createOrderMenu(Menu.ICE_CREAM, 1),
+                createOrderMenu(Menu.CHOCOLATE_CAKE, 1)
+        );
+        OrderMenus orderMenus = new OrderMenus(menus);
+
+        EventPanner eventPanner = new EventPanner(date, orderMenus);
+
+        //when
+        int paymentPrice = eventPanner.getPaymentPrice();
+
+        //then
+        assertThat(paymentPrice).isEqualTo(predictionPaymentPrice);
+    }
+
+    private OrderMenu createOrderMenu(Menu menu, int amount) {
+        return new OrderMenu(menu, amount);
+    }
+
+}
