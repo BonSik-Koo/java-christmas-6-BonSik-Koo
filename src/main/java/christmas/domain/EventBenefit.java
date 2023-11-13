462c8ea780ce2788f52570ae6741849edcff464c
BonSik <rnqhstlr2297@gmail.com>
2023-11-14 02:26:17 +0900

    feat: 이벤트 참여 여부 검증 구현

--- src/main/java/christmas/domain/EventBenefit.java
+++ src/main/java/christmas/domain/EventBenefit.java
@@ -1,15 +1,25 @@
 package christmas.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class EventBenefit {
+    private final static int EVENT_BENEFIT_STANDARD_PRICE = 10000;
     private final static int PRESENT_MENU_STANDARD_PRICE = 120000;
     private final List<EventDiscount> eventDiscounts;
     private final Menu presentMenu;
 
     public EventBenefit(Date date, OrderMenus orderMenus) {
-        this.eventDiscounts = EventDiscountClassifier.generateEventBenefits(date, orderMenus);
+        this.eventDiscounts = determineEventDiscount(date, orderMenus);
         this.presentMenu = determinePresentMenu(orderMenus);
+    }
+
+    private List<EventDiscount> determineEventDiscount(Date date, OrderMenus orderMenus) {
+        int totalOrderPrice = orderMenus.calculateTotalPrice();
+        if (totalOrderPrice >= EVENT_BENEFIT_STANDARD_PRICE) {
+            return EventDiscountClassifier.generateEventBenefits(date, orderMenus);
+        }
+        return new ArrayList<>();
     }
 
     private Menu determinePresentMenu(OrderMenus orderMenus) {
