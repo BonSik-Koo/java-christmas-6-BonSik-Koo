7d63ffd471f33b6cda28394d477110486633177d
BonSik <rnqhstlr2297@gmail.com>
2023-11-14 15:55:33 +0900

    feat: 주문 메뉴와 개수 입력 기능 구현

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,9 +1,13 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
 
 public class InputView {
     private final static String READ_DATE_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    private final static String READ_MENU_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+    private final static String COMMA_DELIMITER = ",";
 
     public int readDate() {
         System.out.println(READ_DATE_MESSAGE);
@@ -11,6 +15,14 @@ public class InputView {
         InputValidator.validateNumeric(date);
 
         return Integer.parseInt(date);
+    }
+
+    public List<String> readMenu() {
+        System.out.println(READ_MENU_MESSAGE);
+        String menus = Console.readLine();
+        InputValidator.validateMenuPattern(menus);
+
+        return Arrays.asList(menus.split(COMMA_DELIMITER));
     }
 
 }
