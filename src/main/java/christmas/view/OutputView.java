bc20141677a64e483cc1984d212592fa3cda3bbe
BonSik <rnqhstlr2297@gmail.com>
2023-11-14 16:13:35 +0900

    feat: 이벤트 시작 메시지 출력 기능 구현

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,13 @@
+package christmas.view;
+
+public class OutputView {
+    private final static String EVENT_START_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+
+    public OutputView() {
+    }
+
+    public void printEventStartMessage() {
+        System.out.println(EVENT_START_MESSAGE);
+    }
+
+}
